708096783a02f257bcccce71de36baf666cd9346
Punkinut <heypunkinut@gmail.com>
2021-05-04 13:17:11 +1000

    Special character check validation

--- Develop/lib/Letter.js
+++ Develop/lib/Letter.js
@@ -1,5 +1,15 @@
 class Letter {
-  
+  constructor(letter) {
+      this.letter = letter;
+      this.special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '[', ']', ';', '?']
+      this.special.forEach((char) => {
+        if(letter === char) {
+            this.visible = true;
+        } else {
+            this.visible = false;
+        }
+      })
+  }
 }
 
 module.exports = Letter;
